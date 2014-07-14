919482925861ce88509efc8adecba49e24989338
Chenliang Xu <luckyrand@gmail.com>
2014-07-14 16:05:44 -0500

    track deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+if [ -n "$(git status --porcelain)" ]
+then
+    echo 'Please commit your change first!!!'
+    exit 1
+fi
+
+SRC_COMMIT_ID=$(git rev-parse HEAD)
+
+rm -rf build/*
+wintersmith build
+
+( cd build
+  git add --all * ## The --all option is need to track removals
+  git commit -m "deploy from commit ${SRC_COMMIT_ID}"
+  git push
+  )
