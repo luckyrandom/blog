2ad22993d64e177332b3b6e282393f7dab5e2443
Chenliang Xu <luckyrand@gmail.com>
2017-03-15 01:09:18 -0700

    Fix highlight

--- contents/javascript/highlight-pandoc.js
+++ contents/javascript/highlight-pandoc.js
@@ -2,11 +2,22 @@ $(document).ready(function() {
     $("pre code")
         .each(function(i, block){
             $(block).html(function(index, html) {
-                var lang = $(this).parent().attr("class");
-                if (lang)
-                    return hljs.highlight(lang, $(this).text(), true).value;
-                else
-                    return html;
+                var clazz = $(this).parent().attr("class");
+                if (clazz) {
+                    var possibleLangs = clazz.split(" ");
+                    for (var i = 0; i < possibleLangs.length; i++) {
+                        var lang = possibleLangs[i];
+                        if (lang && lang != "sourceCode") {
+                            try {
+                                return hljs.highlight(lang, $(this).text(), true).value;
+                            } catch (e) {
+                                // Skip error and try the next possible lang name
+                                console.log(e)
+                            }
+                        }
+                    }
+                }
+                return html;
             });
         });
 });
