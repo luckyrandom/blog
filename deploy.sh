4dbe26f07364a5029873731c79fa679f60b7f780
Chenliang Xu <luckyrand@gmail.com>
2017-03-15 00:11:57 -0700

    deploy set up git when needed

--- deploy.sh
+++ deploy.sh
@@ -14,9 +14,19 @@ SRC_COMMIT_ID=$(git rev-parse HEAD)
 source build.sh
 
 ( cd build
+
+  ## Set up git to track the gh-pages branch if neesecary
+  if [ ! -d ".git" ]; then
+     git init
+     git remote add origin git@github.com:luckyrandom/blog.git -t gh-pages
+     git fetch origin
+     git reset --soft origin/gh-pages
+  fi
+
+  ## Push all files to the gh-pages branch
   git add --all * ## The --all option is need to track removals
   git commit -m "deploy from commit ${SRC_COMMIT_ID}"
-  git push
+  git push --set-upstream origin gh-pages
   )
 
 git push
